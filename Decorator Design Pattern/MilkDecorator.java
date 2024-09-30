f907c712790d0049f6f2067f3201c521e0f30cac
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2024-09-30 22:20:55 +0530

    Create MilkDecorator.java

---
+++ Decorator Design Pattern/MilkDecorator.java
@@ -0,0 +1,17 @@
+ // (Concrete Decorator)
+
+public class MilkDecorator extends AddonDecorator {
+    public MilkDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription() + ", Milk";
+    }
+
+    @Override
+    public double cost() {
+        return coffee.cost() + 0.30;
+    }
+}
