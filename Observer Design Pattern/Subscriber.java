c21ede84de81fae12d3f08053aeb7584144c4587
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2024-09-30 22:04:32 +0530

    Create Subscriber.java

---
+++ Observer Design Pattern/Subscriber.java
@@ -0,0 +1,14 @@
+// Concrete Observer 
+
+public class Subscriber implements Observer {
+    private String name;
+
+    public Subscriber(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(String news) {
+        System.out.println(name + " received news update: " + news);
+    }
+}
