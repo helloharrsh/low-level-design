e69e7e13ed1c870313acd8b9db5f92500ed290f0
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2024-09-30 19:23:00 +0530

    Create Concrete Strategies.java

---
+++ Strategy Design Pattern/Concrete Strategies.java
@@ -0,0 +1,26 @@
+// BubbleSortStrategy
+public class BubbleSortStrategy implements SortingStrategy {
+	@Override
+	public void sort(int[] array) {
+		// Implement Bubble Sort algorithm
+		System.out.println("Sorting using Bubble Sort");
+	}
+}
+
+// MergeSortStrategy
+public class MergeSortStrategy implements SortingStrategy {
+	@Override
+	public void sort(int[] array) {
+		// Implement Merge Sort algorithm
+		System.out.println("Sorting using Merge Sort");
+	}
+}
+
+// QuickSortStrategy
+public class QuickSortStrategy implements SortingStrategy {
+	@Override
+	public void sort(int[] array) {
+		// Implement Quick Sort algorithm
+		System.out.println("Sorting using Quick Sort");
+	}
+}
