31cc26698c813f08df65c7da6c4419ce6b1ea2b1
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2024-09-30 22:21:34 +0530

    Create SugarDecorator.java

---
+++ Decorator Design Pattern/SugarDecorator.java
@@ -0,0 +1,17 @@
+//  (Concrete Decorator)
+
+public class SugarDecorator extends AddonDecorator {
+    public SugarDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription() + ", Sugar";
+    }
+
+    @Override
+    public double cost() {
+        return coffee.cost() + 0.20;
+    }
+}
